b96636a1a4df4d642bc7782db207e6fe375c1514
eelcovendrik <87235353+eelcovendrik@users.noreply.github.com>
2021-08-21 15:46:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-// Dit is een grote som
-/* console.log(1230941 * 1823794);
-console.log(637263 / 54); */
+const age = 5
+if (age > 18) {
+    console.log("Deze persoon mag naar binnen")
+} else {
+    console.log("Deze persoon is te jong en moet wordt vriendelijk verzocht buiten te blijven")
+}
+
+const isFemale = false
+if (isFemale) {
+    console.log("De persoon is een vrouw")
+} else {
+    console.log("Deze persoon is een man")
+}
+
+const driverStatus = "bob"
+
+if (driverStatus == "bob") {
+    console.log("Deze persoon mag rijden")
+} else {
+    console.log("Deze persoon mag niet rijden")
+}
