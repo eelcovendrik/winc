3cb398efd2b9f02bf8cebab81c4bd3957ca65ae6
eelcovendrik <87235353+eelcovendrik@users.noreply.github.com>
2021-08-22 12:37:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,80 @@
-const age = 20;
-const isFemale = true;
-const driverStatus = "bob";
-const firstName = "Kees";
-const totalAmount = 26;
+let person = {
+    name: 'Eelco',
+    age: 37
+};
 
-if (age > 18) {
-    console.log("Deze persoon mag naar binnen");
-} else {
-    console.log("Helaas, je bent te jong. Je wordt vriendelijk verzocht buiten te blijven");
-}
+console.log(person);
 
-if (isFemale) {
-    console.log("Je ticket is geldig voor Ladies Night");
-} else {
-    console.log("Je ticket is niet geldig voor Ladies Night");
-}
+console.log(person.name);
+console.log(person['name']);
+console.log(person.age);
+console.log(person['age']);
 
-if (driverStatus == "bob") {
-    console.log("Deze persoon mag rijden");
-} else {
-    console.log("Deze persoon mag niet rijden");
-}
+let evulations = [7, 10, 9];
+console.log(evulations);
 
-if (age >= 18 && age <= 25 ) {
-    console.log("Je krijgt 50% korting");
-} else {
-    console.log("Je krijgt helaas geen korting");
-}
+let kleuren = ['groen', 'blauw', 'rood'];
+console.log(kleuren);
+console.log(kleuren.length);
+console.log(kleuren[0]);
+console.log(kleuren[kleuren.length - 1]);
+kleuren.push('geel')
+console.log(kleuren);
+kleuren.push('5');
+console.log(kleuren);
+kleuren.push({ greeting: "hi ik ben een object" });
+console.log(kleuren);
+console.log(kleuren[5]);
 
-if (firstName == "Bram" || firstName == "Sarah") {
-    console.log("Je krijgt een gratis biertje van ons");
-} else {
-    console.log("Helaas, je krijgt geen gratis bier van ons");
+const catBreeds = [{
+    name: "Abyssinian",
+    description: "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.",
+    dog_friendly: 4,
+    energy_level: 5,
+    life_span: "14 - 15",
+    origin: "Egypt",
+    temperament: ["Active", "Energetic", "Independent", "Intelligent", "Gentle"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/Abyssinian_(cat)",
+    food: {
+        favourite_food: "fish",
+        medium_liked_food: "dried fruits",
+        disliked_food: "walnuts"
+    }
+},
+{
+    name: "Aegean",
+    description: "Native to the Greek islands known as the Cyclades in the Aegean Sea, these are natural cats, meaning they developed without humans getting involved in their breeding. As a breed, Aegean Cats are rare, although they are numerous on their home islands. They are generally friendly toward people and can be excellent cats for families with children.",
+    dog_friendly: 4,
+    energy_level: 53,
+    life_span: "9- 12",
+    origin: "Greece",
+    temperament: ["Affectionate", "Social", "Intelligent", "Playful", "Active"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/Aegean_cat",
+    food: {
+        favourite_food: "tuna",
+        medium_liked_food: "canned food",
+        disliked_food: "all fruits"
+    }
+},
+{
+    name: "American Bobtail",
+    description: "American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.",
+    dog_friendly: 5,
+    energy_level: 3,
+    life_span: "11 - 15",
+    origin: "United States",
+    temperament: ["Intelligent", "Interactive", "Lively", "Playful", "Sensitive"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/American_Bobtail",
+    food: {
+        favourite_food: "meaty things",
+        medium_liked_food: "tuna",
+        disliked_food: "canned food"
+    }
 }
+]
 
-if (totalAmount > 100) {
-    console.log("Je krijgt een gratis fles champagne");
-} else if (totalAmount > 50) {
-    console.log("Je krijgt een gratis porties nachos");
-} else if (totalAmount > 25) {
-    console.log("Je krijgt gratis bitterballen");
-} else {
-    console.log("Je krijgt geen niks");
-}
+console.log('Naam kattensoort 3:', catBreeds[2].name);
+console.log('Energylevel kattensoort 1:', catBreeds[0].energy_level);
+console.log('Eerste temperament kat 2:', catBreeds[1].temperament[0]);
+console.log('Laatste temperament kat 3:', catBreeds[2].temperament[4]);
+console.log('Favo voedsel van kat 3:', catBreeds[2].food.favourite_food);
