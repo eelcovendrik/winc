d67626084973054cbec7572bdef4175f1ccb3ecc
eelcovendrik <87235353+eelcovendrik@users.noreply.github.com>
2021-08-23 18:49:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-// Writing or 'defining'a function
-const makePizza = function () {
-    console.log('Pak bodem');
-    console.log('Beleg met tomaten');
-    console.log('Beleg met kaas');
-    console.log('Doe het in de oven');
+const paintingWall = function(color) {
+    console.log("The wall has been painted " + color)
 };
 
-// Using or 'calling' a function
-makePizza();
-makePizza();
-makePizza();
+paintingWall("green");
+paintingWall('yellow');
+paintingWall();
 
-const makeSushi = function () {
-    console.log('Kook rijst');
-     console.log('Snij vis');
-     console.log('Combineer rijst en vis');
- }
+const paintingMoreWalls = function(colorSouthEast, colorNorth, ) {
+    console.log("The north wall has been painted " + colorNorth);
+    console.log("The south-east wall has been painted " + colorSouthEast);
+};
 
-makePizza();
-makePizza();
-makePizza();
-makeSushi();
-makeSushi();
+
+paintingMoreWalls('yellow','green')
+paintingMoreWalls('green','yellow')
+
+// const heat = function (item) {
+//     console.log("I'm heating " + item);
+
+//     console.log("Ping! Your " + item + " is heated.")
+// };
+
+// const main_course = 'pasta';
+// const dessert = 'cake';
+
+// heat(main_course);
+// heat(dessert);
+
+// const multiply = function (numberA, numberB) {
+//     console.log(numberA * numberB)
+// };
+
+// multiply(3, 4);
+
+// const age = 5;
+// const dogYearsFactor = 7;
+// multiply(age, dogYearsFactor);
+
+// const calculateAverage = function (numbers) {
+//     const berekenGemiddeldee =  numbers.reduce((total,n) => total + n) / numbers.lenght;
+//     console.log(berekenGemiddeldee);
+
+// };
+
+// const grades = [6,7,8,9,5,6,7,8];
+// calculateAverage(grades);
+
+// numbers.red
+
