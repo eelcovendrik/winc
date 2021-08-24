2fca615bf777ca91fc6bf7384624ae638dd24b16
eelcovendrik <87235353+eelcovendrik@users.noreply.github.com>
2021-08-24 19:34:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,29 @@
-const paintingWall = function(color) {
-    console.log("The wall has been painted " + color)
-};
+const checkBiggerThan100 = function(number) {
+if (number > 100) {
+    return "true";
+} return "false";
+}
 
-paintingWall("green");
-paintingWall('yellow');
-paintingWall();
+const result = checkBiggerThan100(500);
+console.log(result);
 
-const paintingMoreWalls = function(colorSouthEast, colorNorth, ) {
-    console.log("The north wall has been painted " + colorNorth);
-    console.log("The south-east wall has been painted " + colorSouthEast);
-};
+const bouncerBot = function (maximumNumber, currentNumber, age) {
+    if (currentNumber > maximumNumber) {
+        return "it's too busy now, come back later"
+    } else if (age < 18) {
+        return "this is a club for adults";
+    }
+    else return "come in";
+}
 
+const answer = bouncerBot(70, 80, 17);
+console.log(answer);
 
-paintingMoreWalls('yellow','green')
-paintingMoreWalls('green','yellow')
+const averageCalculation = function (number1, number2, number3, number4, number5) {
+    average = (number1 + number2 + number3 + number4 + number5) / 5;
+    averageRound = Math.round(average);
+    return averageRound;
+}
 
-// const heat = function (item) {
-//     console.log("I'm heating " + item);
-
-//     console.log("Ping! Your " + item + " is heated.")
-// };
-
-// const main_course = 'pasta';
-// const dessert = 'cake';
-
-// heat(main_course);
-// heat(dessert);
-
-// const multiply = function (numberA, numberB) {
-//     console.log(numberA * numberB)
-// };
-
-// multiply(3, 4);
-
-// const age = 5;
-// const dogYearsFactor = 7;
-// multiply(age, dogYearsFactor);
-
-// const calculateAverage = function (numbers) {
-//     const berekenGemiddeldee =  numbers.reduce((total,n) => total + n) / numbers.lenght;
-//     console.log(berekenGemiddeldee);
-
-// };
-
-// const grades = [6,7,8,9,5,6,7,8];
-// calculateAverage(grades);
-
-// numbers.red
-
+const resultAverageCalc = averageCalculation(2, 2, 5, 2, 2);
+console.log(resultAverageCalc);
