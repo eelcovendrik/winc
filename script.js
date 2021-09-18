7591b4c8bf567c7c3ea609f74c9246a7804d828a
eelcovendrik <87235353+eelcovendrik@users.noreply.github.com>
2021-09-18 11:43:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var colors = ['yellow', 'blue', 'red', 'orange'];
+
+let i = 0;
+while (i < colors.length) {
+  console.log(colors[i]);
+  i++;
+}
+
+for (i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+
+colors.forEach(element => console.log(element));
